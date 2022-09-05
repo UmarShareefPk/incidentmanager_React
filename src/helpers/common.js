9e8efdfefff7e7dc22382dc88b489853a849bb70
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-09-05 14:23:08 +0500

    Update common.js

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -1,11 +1,13 @@
 import moment from "moment";
 
 export const setDateTime = (date) => {
-    return date;
+   // return date;
     try{
         let newDate = moment(date).format("YYYY-MM-DD h:mm:ss A");
-        newDate = new Date(newDate + " UTC");    
-        return newDate.toString();
+        newDate = new Date(newDate + " UTC");
+       // console.log("date", date);    
+       // console.log("newDate", newDate);    
+        return newDate;
     }
     catch(err){
             console.log("Error in dates", err);
