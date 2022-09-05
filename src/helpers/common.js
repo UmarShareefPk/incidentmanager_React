ddfc4075d683d7ab350bf420e02ef6e87d17a78e
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-09-05 13:57:33 +0500

    date conversion in try catch

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -1,7 +1,14 @@
 import moment from "moment";
 
 export const setDateTime = (date) => {
-    let newDate = moment(date).format("YYYY-MM-DD h:mm:ss A");
-    newDate = new Date(newDate + " UTC");    
-    return newDate.toString();
+    try{
+        let newDate = moment(date).format("YYYY-MM-DD h:mm:ss A");
+        newDate = new Date(newDate + " UTC");    
+        return newDate.toString();
+    }
+    catch(err){
+            console.log("Error in dates", err);
+            return date.toString();
+    }
+   
 }
